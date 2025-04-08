35313b265c1a36778c85e86fa97470b6d4476d03
Shyam Vishwakarma <shyamvswkrma@gmail.com>
2025-04-08 15:28:56 +0530

    initial commit

---
+++ src/test/java/com/imaging/app/ImagingApplicationTests.java
@@ -0,0 +1,11 @@
+package com.imaging.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagingApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
