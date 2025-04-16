c7c634157cd5d6512a2d425023e2a3a86b3a3fbe
Shyam Vishwakarma <shyamvswkrma@gmail.com>
2025-04-16 23:18:37 +0530

    feat: add wallet config, model, service, repo, exception and controller

---
+++ src/main/java/com/imaging/app/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.imaging.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/health")
+public class HealthController {
+    @GetMapping
+    public String health() {
+        return "logged in!";
+    }
+}
---
+++ src/main/java/com/imaging/app/controller/WalletController.java
@@ -0,0 +1,29 @@
+package com.imaging.app.controller;
+
+import com.imaging.app.exception.WalletNotFoundException;
+import com.imaging.app.model.Wallet;
+import com.imaging.app.service.WalletService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/wallet")
+@RequiredArgsConstructor
+public class WalletController {
+    private final WalletService walletService;
+
+    @GetMapping
+    public ResponseEntity<Wallet> getWallet(@RequestParam String userId) {
+        Wallet wallet = walletService.getWallet(userId);
+        if (wallet == null) {
+            throw new WalletNotFoundException(userId);
+        } else return ResponseEntity.ok(wallet);
+    }
+
+    @GetMapping("/credits")
+    public ResponseEntity<?> getCredits(@RequestParam String userId) {
+        double credits = walletService.getCredit(userId);
+        return ResponseEntity.ok(credits);
+    }
+}
